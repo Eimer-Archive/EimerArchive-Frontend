1e57624ddd3de4072058b25d9fcf304fb7a97f00
JustDoom <justdoomdev@gmail.com>
2022-07-24 13:25:52 +1000

    Update index.vue

--- pages/archive/[id]/index.vue
+++ pages/archive/[id]/index.vue
@@ -1,24 +1,49 @@
 <template>
-  <div class="block">
-    <h1 class="title"><b>CraftBukkit</b></h1>
-    <p class="description">A really good server software that allows plugins and all that jazz</p>
-  </div>
-  <div class="block">
-    <h1 class="title">Beta 1.7.3</h1>
-    <div class="top-bar">
-      <p class="version">Build: 1093</p>
-      <button class="download-button" style="margin-top: 3px">Download</button>
+  <div v-if="isLoaded">
+    <div class="block">
+      <h1 class="title"><b>{{ this.project.name }}</b></h1>
+      <p class="description">{{ this.project.description }}</p>
     </div>
-    <div class="top-bar">
-      <p class="version">Build: 1060</p>
-      <button class="download-button" style="margin-top: 3px">Download</button>
+    <div class="block">
+      <h1 class="title">Beta 1.7.3</h1>
+      <div class="top-bar">
+        <p class="version">Build: 1093</p>
+        <button class="download-button" style="margin-top: 3px">Download</button>
+      </div>
+      <div class="top-bar">
+        <p class="version">Build: 1060</p>
+        <button class="download-button" style="margin-top: 3px">Download</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'index',
+  data () {
+    return {
+      isLoaded: false,
+      project: {},
+    }
+  },
+  async beforeCreate () {
+    const config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+
+    try {
+      const res = await axios.get('http://localhost:8080/archive/' + this.$route.params.id, config)
+      this.project = res.data;
+      this.isLoaded = true;
+    } catch (e) {
+      console.log(e)
+    }
+  },
 }
 </script>
 
@@ -29,15 +54,18 @@ export default {
   padding: 15px;
   margin: 16px 0;
 }
+
 .title {
   font-size: 20px;
   text-decoration-thickness: auto;
   color: #ffffff;
 }
+
 .description {
   color: white;
   text-decoration-thickness: 1px;
 }
+
 .top-bar {
   border-style: solid;
   color: dimgray;
@@ -46,6 +74,7 @@ export default {
   margin-top: 10px;
   padding-top: 5px;
 }
+
 .download-button {
   color: white;
   border-radius: 3px;
@@ -54,6 +83,7 @@ export default {
   font-size: 13px;
   padding: 1px 4px;
 }
+
 .version {
   color: white;
 }
