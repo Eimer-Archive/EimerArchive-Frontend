f62669ef238a9d7efd68e9fb2d7384e5c0fda360
JustDoom <61824552+JustDoom@users.noreply.github.com>
2022-12-02 22:23:09 +1000

    Update error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-block">
-    <h1 class="text-4xl font-bold text-white">404</h1>
-    <p class="text-white">The page you are looking for does not exist.</p>
+    <h1 class="text-4xl font-bold text-white">{{ error.message }}</h1>
+    <NuxtLink class="text-white" to="/">Oh no! It seems there was an error. Click me to go home</NuxtLink>
   </div>
 </template>
 
@@ -17,6 +17,16 @@ import Header from '../components/header'
 export default {
   components: {
     Header,
+  },
+  props: {
+    error: {
+      type: Object,
+      default() {
+        return {
+          message: 'Unknown error',
+        }
+      },
+    },
   },
 }
 </script>
