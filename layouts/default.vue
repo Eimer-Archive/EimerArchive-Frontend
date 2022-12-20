cd0839773d416735795eeede05d69725aebca3af
JustDoom <61824552+JustDoom@users.noreply.github.com>
2022-12-20 20:50:23 +1000

    shrink width of text containers a bit

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col min-h-screen">
     <Header/>
     <div class="basis-0 grow">
-      <div class="ml-[50%] my-8 transform -translate-x-1/2 w-[80%]">
+      <div class="ml-[50%] my-8 transform -translate-x-1/2 w-[70%]">
         <nuxt/>
       </div>
     </div>
