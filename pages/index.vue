e3e0b3e87b1a049458efb71474613d73af15a132
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-06-15 13:12:30 +1000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-block">
-    <h1>Eimer Archive</h1>
+    <h1 class="font-bold text-3xl">Eimer Archive</h1>
     Eimer archive is an archive targeting Minecraft server software and plugins. It provides access to files that are no
     longer readily available on the internet, such as old versions of plugins and server software. If you would like to
     help out, please join the <a href="https://discord.gg/k8RcgxpnBS">Eimer Archive Discord</a>. To start finding files,
